1d330e36dda1f1a20cd9a5c1ec8f81233cbed418
stmr <samik4184@gmail.com>
2014-03-06 23:17:09 +0100

    prvni commit

---
+++ firstWinForms/Form1.Designer.cs
@@ -0,0 +1,39 @@
+﻿namespace firstWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžadovaná proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolnit všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by měl být spravovaný prostředek odstraněn, jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "Form1";
+        }
+
+        #endregion
+    }
+}
+
